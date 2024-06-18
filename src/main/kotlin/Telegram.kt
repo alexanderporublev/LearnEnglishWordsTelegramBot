389855afcf102ea91a4ca51f229b325d0e3c0938
Aleksandr Porublev <a.porublev@red-soft.ru>
2024-06-18 20:43:06 +0300

    Отправка ответа на сообщение в чат

--- src/main/kotlin/Telegram.kt
+++ src/main/kotlin/Telegram.kt
@@ -1,31 +1,31 @@
 package org.example
 
-import java.net.URI
-import java.net.http.HttpClient
-import java.net.http.HttpRequest
-import java.net.http.HttpResponse
-
-fun getUpdates(botToken: String, updateId: Int): String {
-    val urlGetUpdates = "https://api.telegram.org/bot$botToken/getUpdates?offset=$updateId"
-    val client = HttpClient.newBuilder().build()
-
-    val request = HttpRequest.newBuilder().uri(URI.create(urlGetUpdates)).build()
-    val response: HttpResponse<String> = client.send(request, HttpResponse.BodyHandlers.ofString())
-
-    return response.body()
+fun getChatsWhichSentCommand(updates: String, command: String): String? {
+    val result = emptyList<String>().toMutableList()
+    val messageTextRegex: Regex = "\"text\":\"(.+?)\"".toRegex()
+    val matchTextResult: MatchResult? = messageTextRegex.find(updates)
+    if (matchTextResult?.groups?.get(1)?.value == command) {
+        val chatIdRegex: Regex = "\"chat\":\\{\"id\":(\\d+),".toRegex()
+        val matchChatIdResult: MatchResult? = chatIdRegex.find(updates)
+        return matchChatIdResult?.groups?.get(1)?.value
+    }
+    return null
 }
 
 fun main(args: Array<String>) {
     val botToken = args[0]
     var updateId = 0
-
-    while (true){
+    val botService = TelegramBotService(botToken)
+    while (true) {
         try {
             Thread.sleep(2000)
-            val updates: String = getUpdates(botToken, updateId)
+            val updates: String = botService.getUpdates(updateId)
             println(updates)
             updateId = getLastUpdateId(updates)
-        } catch (e: Exception){
+            val chat = getChatsWhichSentCommand(updates, "hello")
+            if (chat != null)
+                botService.sendMessage(chat, "Hello")
+        } catch (e: Exception) {
             continue
         }
     }
